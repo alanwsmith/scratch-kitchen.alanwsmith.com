4374ca516789ef64cb595e7532413651654bdb18
Alan Smith <commit@alanwsmith.com>
2022-04-22 21:24:15 -0400

    Added Welcome video

--- site/pages/index.js
+++ site/pages/index.js
@@ -18,48 +18,50 @@ const some_object = { key: 'value' }
       />
       <h1>Welcome To Scratch Lab</h1>
       <p>
-        Hi there! My name is Alan and I happen to have a wonderful niece
-        who&apos;s asked me to help her become a computer genius. Drawing on my
-        thirty years of experience in the world of computers, I&apos;m building
-        this site to do just that. But, we&apos;re not selfish. You&apos;re
-        welcome to join us if you&apos;d like to come along for the ride and
-        become a computer genius too.
+        Hi there! I&apos;m Uncle Alan and I&apos;m teaching my niece to become a
+        computer genius. You&apos;re welcome to tag along if you&apos;d like to
+        become one too.
       </p>
       <p>
         This site is mostely videos. So, check this one out to learn more about
         what&apos;s going on here and see if you&apos;re interested in joining
         in.
       </p>
-      <p>TODO: Make the video that goes here. Key points:</p>
+      <iframe
+        src="https://player.vimeo.com/video/702250919?h=81ba484d20"
+        width="640"
+        height="416"
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+
+      <h3>
+        Note: this page is a draft prototype that&apos; under construction. The
+        second video is in production and on the way
+      </h3>
+
+      <h3>Draft Notes</h3>
+      <ul>
+        <li>See Grimoire notes for more details</li>
+      </ul>
+
+      <h3>Ideas, Drafts, Notes, etc...</h3>
       <ul>
         <li>
-          The site is free and always will be. (Figure out how detailed to get
-          about open source)
-        </li>
-        <li>There are no ads on the site.</li>
-        <li>
-          The computer industry has been great to me and this is an effort to
-          pay it forward. I&apos;m able to pay for the site myself so
-          that&apos;s what I&apos;m doing
+          Split things into three types: Ingredients, Concoctions, and Tidbits
         </li>
         <li>
-          This isn&apos;t a class or a bootcamp or a workshop. It&apos;s more
-          like a garage where your excentric uncle is showing you how to use
-          power tools
+          Ingredients and Concoctions probalby will break down into basic and
+          advanced?
         </li>
         <li>
-          Scratch is for kids in the same way a Pixar movie is. Which is to say,
-          it&apos;s not just for kids at all. It&apos;s setup perfectly to show
-          folks anywhere from seven to seventy how to program.{' '}
+          Ingredients are single, refined examples about one specific thing
         </li>
-        <li>See Grimoire notes for more details</li>
+        <li>Concoctions are combinations of ingredients to make things</li>
         <li>
-          As the certified expert, I hearby give you permission to play around
-          with every single piece of Scratch and break it all with the guarantee
-          that the safety bumpers it has will prevent you from doing anything to
-          break your computer or anything else other than whatever the specific
-          things is that you&apos;re workign on and we can always get that
-          working again with a little effort.
+          Tidbits are little pieces of information about programming and ways to
+          think about it
         </li>
       </ul>
     </>
